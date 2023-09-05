56d3d5abe4426f000e87eea522fbef9d68524752
kolopostikum <52316973+kolopostikum@users.noreply.github.com>
2023-09-05 15:00:35 +0300

    Шифр Бэкона
    Посчитать количество различных подстрок строки с использованием хэширования за O(N 3).

---
+++ BeconShifr/BeconShifr/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeconShifr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var variables = new HashSet<string>();
+            SearchCountBecon(input, variables, 0, input.Length);
+            var result = variables.Count();
+            Console.WriteLine(result);
+        }
+
+        private static void SearchCountBecon(string input, HashSet<string> variables, int indexFirst, int indexLast)
+        {
+            if (indexLast <= 0)
+                return;
+            var substring = input.Substring(indexFirst, indexLast);
+            if (!variables.Contains(substring))
+                variables.Add(substring);
+            SearchCountBecon(input, variables, indexFirst + 1, indexLast - 1);
+            SearchCountBecon(input, variables, indexFirst, indexLast - 1);
+        }
+    }
+}
