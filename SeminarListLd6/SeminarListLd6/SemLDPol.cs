dce6192f2b8d046004dd5f5dfa8bfd34add0b78f
kolopostikum <52316973+kolopostikum@users.noreply.github.com>
2023-09-05 14:57:20 +0300

    Add files via upload
    Даны две строки длины N. Найдите их наибольшую общую подстроку.

---
+++ SeminarListLd6/SeminarListLd6/SemLDPol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace SeminarListLd6
+{
+    class SemLDPol
+    { 
+        public static List<string> GetPolinromes(string input)
+        {
+            const int p = 31;
+            Int64 hash = 0;
+            Int64 p_pow = 1;
+            for (var i = 0; i < input.Length; ++i)
+            {
+                hash += (input[i] - 'a' + 1) * p_pow;
+                p_pow *= p;
+            }
+        }
+    }
+}
---
+++ SeminarListLd6/SeminarListLd6/SemLd.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace SeminarListLd6
+{
+    class SemLd
+    { 
+        public static string getMaxSubstring(string input1, string input2)
+        {
+            var length = input1.Length;
+            var arrChar = new int[length, length];
+            int maxValue = 0;
+            int maxI = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if ((i == 0) || (j == 0))
+                    {
+                        if (input1[i] == input2[j])
+                            arrChar[i, j] = 1;
+                        if (arrChar[i, j] > maxValue)
+                        {
+                            maxValue = arrChar[i, j];
+                            maxI = i;
+                        }
+                    }
+                    else if (input1[i] == input2[j])
+                    {
+                        arrChar[i, j] = arrChar[i - 1, j - 1] + 1;
+                        if (arrChar[i, j] > maxValue)
+                        {
+                            maxValue = arrChar[i, j];
+                            maxI = i;
+                        }
+                    }
+                }
+            }
+            if (maxValue > 0)
+                return input1.Substring(maxI + 1 - maxValue, maxValue);
+            return null;
+        }
+
+    }
+}
